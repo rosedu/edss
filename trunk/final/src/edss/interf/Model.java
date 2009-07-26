6bc405062d0b0f54b06ec0278bfb6d6c14640eee
Adrian Sendroiu <noemail@rosedu.org>
2009-07-26 14:23:43 +0000

    git-svn-id: svn://svn.rosedu.org/edss@89 fb1e35bc-5476-41b4-a6fd-87af01637d8f

--- trunk/final/src/edss/interf/Model.java
+++ trunk/final/src/edss/interf/Model.java
@@ -4,6 +4,7 @@ public interface Model {
 
 	Piece getPiece(String id);
 	String getSVG();
+	String getId();
 	void update();
 	void createPiece(String category, String subcategory, String name);
 	void addPiece(int x, int y);
