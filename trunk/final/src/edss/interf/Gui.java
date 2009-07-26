aa348dd65799d3f6244ddac13f5ea6ea7ef51709
Octavian Rinciog <noemail@rosedu.org>
2009-07-26 09:05:59 +0000

    git-svn-id: svn://svn.rosedu.org/edss@66 fb1e35bc-5476-41b4-a6fd-87af01637d8f

--- trunk/final/src/edss/interf/Gui.java
+++ trunk/final/src/edss/interf/Gui.java
@@ -1,7 +1,11 @@
 package edss.interf;
 
+import javax.swing.JPanel;
+
 public interface Gui {
 
 	void editPieceProperties(Piece piece);
-
+	JPanel getCenterPanel();
+	JPanel getLeftPreview();
+	boolean hasFrames();
 }
