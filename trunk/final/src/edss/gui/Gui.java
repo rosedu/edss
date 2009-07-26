39c692341805017d39c45a9deeb1256c496a3c87
Octavian Rinciog <noemail@rosedu.org>
2009-07-26 07:53:36 +0000

    git-svn-id: svn://svn.rosedu.org/edss@60 fb1e35bc-5476-41b4-a6fd-87af01637d8f

--- trunk/final/src/edss/gui/Gui.java
+++ trunk/final/src/edss/gui/Gui.java
@@ -245,7 +245,7 @@ public class Gui implements edss.interf.Gui{
 			key = KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.SHIFT_MASK);
 			fSaveAs.setAccelerator(key);
 			
-			JMenuItem fExportToPdf = new JMenuItem("Export to pdf",new ImageIcon("Icons\\exportToPdf.jpg"));
+			JMenuItem fExportToPdf = new JMenuItem("Export to pdf", new ImageIcon("Icons\\exportToPdf.jpg"));
 			fExportToPdf.addActionListener(new ActionListener() 
 			{	public void actionPerformed(ActionEvent e)
 				{
