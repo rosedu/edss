0cfa6130471b94e6447a149589251136adad6daa
Octavian Rinciog <noemail@rosedu.org>
2009-07-26 12:51:10 +0000

    git-svn-id: svn://svn.rosedu.org/edss@75 fb1e35bc-5476-41b4-a6fd-87af01637d8f

---
+++ trunk/final/src/edss/canvas/StateConstant.java
@@ -0,0 +1,9 @@
+package edss.canvas;
+
+public class StateConstant {
+	 public final static  int PIECESTATE = 0;
+	 public final static  int MOUSESTATE = 1;
+	 public final static  int DELETESTATE = 2;
+	 public final static  int DRAGSTATE = 3;
+	 
+}
