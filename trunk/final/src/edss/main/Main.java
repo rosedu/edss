33cbbd573dd32d10602369b23b68eba9b0b62e9a
Octavian Rinciog <noemail@rosedu.org>
2009-07-28 08:38:01 +0000

    git-svn-id: svn://svn.rosedu.org/edss@130 fb1e35bc-5476-41b4-a6fd-87af01637d8f

--- trunk/final/src/edss/main/Main.java
+++ trunk/final/src/edss/main/Main.java
@@ -15,10 +15,10 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Mediator mediator = new Mediator();
-		Gui gui = new GuiImpl(mediator);
-		Canvas canvas = new CanvasImpl(mediator); 
-		Model model = new ModelImpl(mediator);
+		// Mediator mediator = new Mediator();
+		Gui gui = new GuiImpl();
+		// Canvas canvas = new CanvasImpl(mediator); 
+		// Model model = new ModelImpl(mediator);
 	}
 
 }
