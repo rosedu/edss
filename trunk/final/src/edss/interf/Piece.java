f568e8c4d301870aea6ecffe2bdc83cb98bbcf55
Adrian Sendroiu <noemail@rosedu.org>
2009-07-28 09:44:02 +0000

    git-svn-id: svn://svn.rosedu.org/edss@139 fb1e35bc-5476-41b4-a6fd-87af01637d8f

--- trunk/final/src/edss/interf/Piece.java
+++ trunk/final/src/edss/interf/Piece.java
@@ -2,5 +2,11 @@ package edss.interf;
 
 
 public interface Piece {
-	
+	String getId();
+	String getName();
+	String getValue();
+	String getSvgURI();
+	int getX();
+	int getY();
+	void setRotated(int i);
 }
