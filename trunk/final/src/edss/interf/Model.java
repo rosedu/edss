00be6807f754357b6f93791403a38b49a7532957
Adrian Sendroiu <noemail@rosedu.org>
2009-07-29 09:10:59 +0000

    git-svn-id: svn://svn.rosedu.org/edss@184 fb1e35bc-5476-41b4-a6fd-87af01637d8f

--- trunk/final/src/edss/interf/Model.java
+++ trunk/final/src/edss/interf/Model.java
@@ -13,10 +13,15 @@ public interface Model {
 	
 	edss.interf.Piece addPiece(int x, int y);
 	String addWire(String piece1, String pin1, String piece2, String pin2, List<? extends Point> points);
+	void delete(String id);
 	
 	void saveScheme(String name);
 	void openScheme(String name, ModelMediator med);
 	
 	void rotate(int r, String id);
 	List<WireInfo> getWiresInfo(String Id);
+	
+	String splitWire(String splitId, int x, int y, List<? extends Point> list1,
+			List<? extends Point> list2, String idStartPiece,
+			String idStartPin, List<? extends Point> newWireList);
 }
--- trunk/final/src/edss/interf/ModelMediator.java
+++ trunk/final/src/edss/interf/ModelMediator.java
@@ -1,9 +1,18 @@
 package edss.interf;
 
+import java.awt.Point;
+import java.util.List;
+
 public interface ModelMediator {
 	
 	void registerModel(Model model);
 
 	void update();
+
+	void removeWire(String id);
+
+	void addNode(int x, int y);
+
+	void addWire(String id, List<? extends Point> points);
 	
 }
