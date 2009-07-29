d529b37230df7cd191a1d36085eeb7a0ed04c823
Octavian Rinciog <noemail@rosedu.org>
2009-07-29 09:28:07 +0000

    git-svn-id: svn://svn.rosedu.org/edss@189 fb1e35bc-5476-41b4-a6fd-87af01637d8f

--- trunk/final/src/edss/canvas/InsertState.java
+++ trunk/final/src/edss/canvas/InsertState.java
@@ -34,13 +34,20 @@ public class InsertState extends State {
 		int roundX = MyMath.roundAtStep(arg0.getX() / canvas.matrix.scale, PointMatrix.CELL_SIZE);
 		int roundY = MyMath.roundAtStep(arg0.getY() / canvas.matrix.scale, PointMatrix.CELL_SIZE);
 		edss.interf.Piece piece = canvas.mediator.addPiece(roundX, roundY);
-		if (piece.getId() != null) {
-			String fileName = canvas.mediator.getSVG();
-			try {
-				canvas.addPiece(canvas.domFactory, fileName, roundX, roundY, piece.getId(), piece.getValue());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		if(piece != null)
+		{
+			System.out.println("val de la mole:  " + piece.getValue());
+			if (piece.getId() != null) 
+			{
+				String fileName = canvas.mediator.getSVG();
+				try 
+				{
+					canvas.addPiece(canvas.domFactory, fileName, roundX, roundY, piece.getId(), piece.getValue());
+				} 
+				catch (IOException e) 
+				{
+					e.printStackTrace();
+				}
 			}
 		}
 	}
