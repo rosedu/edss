f608d9e9df503ec22dce130e2a5b68d65518d388
Mircea Urse <noemail@rosedu.org>
2009-11-24 16:23:35 +0000

    canvas impachetat
    git-svn-id: svn://svn.rosedu.org/edss@210 fb1e35bc-5476-41b4-a6fd-87af01637d8f

--- trunk/final/src/edss/canvas/CanvasImpl.java
+++ trunk/final/src/edss/canvas/CanvasImpl.java
@@ -3,7 +3,6 @@ package edss.canvas;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.event.AdjustmentEvent;
 import java.awt.event.AdjustmentListener;
 import java.awt.geom.AffineTransform;
@@ -17,7 +16,6 @@ import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.OverlayLayout;
-import javax.swing.Scrollable;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
@@ -26,7 +24,6 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.apache.batik.swing.JSVGCanvas;
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -35,56 +32,40 @@ import org.w3c.dom.svg.SVGDocument;
 import org.w3c.dom.svg.SVGLocatable;
 import org.w3c.dom.svg.SVGRect;
 
-
+import edss.canvas.constants.Constant;
+import edss.canvas.pieces.Wire;
+import edss.canvas.transforms.Rotate;
+import edss.canvas.transforms.TransformTag;
 import edss.interf.Canvas;
 import edss.interf.CanvasMediator;
 
-
-@SuppressWarnings("serial")
-public class CanvasImpl extends Constant implements Canvas {
-//	public JFrame frame = new JFrame("Canvas");
+public class CanvasImpl extends Constant implements Canvas
+{
 	public JScrollPane scrollPane;
-	EventListenerImpl eventListener = new EventListenerImpl(this);
-	
-	
-	public static final int WIDTH =2000;
-	public static final int HEIGHT =2000;
-	
-	
-	public JPanel panel = new PanelScroll() {
-		public boolean isOptimizedDrawingEnabled() {
-			return false;
-		}
-	};
-	
-//	static JPanel guiPanel; 
+	public EventListenerImpl eventListener = new EventListenerImpl(this);
 
+	public static final int WIDTH = 2000;
+	public static final int HEIGHT = 2000;
 
-	public CanvasImpl(CanvasMediator mediator) {
-		
-//		public CanvasImpl(CanvasMediator mediator) {
-			
+	public JPanel panel = new PanelScroll();
+
+	public CanvasImpl(CanvasMediator mediator)
+	{
 		this.mediator = mediator;
 		mediator.registerCanvas(this);
 	}
-	
-	public void createNewCanvas(JPanel guiPanel) {
-		 this.domFactory = (SVGDocument) impl.createDocument(svgNS, "svg", null);
-		 setCanvasVariables(guiPanel, domFactory);
+
+	public void createNewCanvas(JPanel guiPanel)
+	{
+		this.domFactory = (SVGDocument) impl.createDocument(svgNS, "svg", null);
+		setCanvasVariables(guiPanel, domFactory);
 	}
-	
+
 	public void setCanvasVariables(JPanel guiPanel, SVGDocument domFactory)
 	{
-//	
-////		}
-//		
-//		this.panel = guiPanel;
-//		panel = guiPanel;
 		this.guiPanel = guiPanel;
 		this.guiPanel.add(panel);
-		System.out.println("!!!gui: " + guiPanel.getParent().getParent());
-		System.out.println("!!!domFact: " + domFactory.getDocumentURI());
-		
+
 		canvas.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		canvas.setDocumentState(JSVGCanvas.ALWAYS_DYNAMIC);
 		canvas.setDocument(domFactory);
@@ -96,8 +77,6 @@ public class CanvasImpl extends Constant implements Canvas {
 		canvas.addMouseListener(eventListener.mouseCanvas);
 		canvas.addMouseMotionListener(eventListener.mouseMotionCanvas);
 		canvas.addKeyListener(eventListener.keyListener);
-//		canvas.setDoubleBuffered(true);
-		
 
 		// init matrix
 		matrix.setDoubleBuffered(true);
@@ -108,112 +87,101 @@ public class CanvasImpl extends Constant implements Canvas {
 		panel.setLayout(new OverlayLayout(panel));
 		panel.add(matrix);
 		panel.add(canvas);
-		System.out.println(panel);
 		scrollPane = new JScrollPane(panel);
 		scrollPane.setAutoscrolls(true);
-		scrollPane
-		.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		scrollPane
-		.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
-		scrollPane.getVerticalScrollBar().addAdjustmentListener(new AdjustmentListener() {
-
-
+		scrollPane.getVerticalScrollBar().addAdjustmentListener(new AdjustmentListener()
+		{
 			@Override
-			public void adjustmentValueChanged(AdjustmentEvent e) {
+			public void adjustmentValueChanged(AdjustmentEvent e)
+			{
 				matrix.repaint();
 			}
-
 		});
-
-
 	}
-	
-	//JScrollPane
-	public Component getCanvas() {
+
+	// JScrollPane
+	public Component getCanvas()
+	{
 		return scrollPane;
 	}
 
+	// what?!
 	@Override
-	public void update() {
-		// TODO Auto-generated method stub
-
+	public void update()
+	{
 	}
 
-	
+	// enter states
 	@Override
-	public void scale(int factor) {
-	// zoom out
-	matrix.scale = factor/100.0;
-
-	AffineTransform at = new AffineTransform();
-	at.scale(matrix.scale, matrix.scale);
-	canvas.setRenderingTransform(at);
-	canvas.repaint();
-
-
-	int canvH = (int) Math.round(HEIGHT * matrix.scale);
-	int canvW = (int) Math.round(WIDTH * matrix.scale);
-	System.out.println("#### Width"+canvW + "Height " + canvH + "Ratio="+ matrix.scale);
-
-	canvas.setPreferredSize(new Dimension(canvW, canvH));
-	canvas.revalidate();
-
-
-	panel.setPreferredSize(new Dimension(canvW, canvH));
-	panel.revalidate();
-
-	//guiPanel.setPreferredSize(new Dimension(canvW, canvH));
-
-
-	guiPanel.validate();
-
-
-
-
-	canvas.repaint();
-	matrix.repaint();
-	}
-	
-	@Override
-	public void enterInsertState() {
+	public void enterInsertState()
+	{
 		stateManager.enterInsertState();
-		
 	}
 
 	@Override
-	public void enterPieceState() {
+	public void enterPieceState()
+	{
 		stateManager.enterPieceState();
-		
+
 	}
 
 	@Override
-	public void enterWireState() {
+	public void enterWireState()
+	{
 		stateManager.enterWireState();
-		
-	}
-	
-	@Override
-	public void enterDeleteState() {
-		stateManager.enterDeleteState();
-		
+
 	}
 
-	
 	@Override
-	public void rotate(int angle) {
+	public void enterDeleteState()
+	{
+		stateManager.enterDeleteState();
+	}
+
+	// transform
+	@Override
+	public void scale(int factor)
+	{
+		// zoom out
+		matrix.scale = factor / 100.0;
+
+		AffineTransform at = new AffineTransform();
+		at.scale(matrix.scale, matrix.scale);
+		canvas.setRenderingTransform(at);
+		canvas.repaint();
+
+		int canvH = (int) Math.round(HEIGHT * matrix.scale);
+		int canvW = (int) Math.round(WIDTH * matrix.scale);
+		canvas.setPreferredSize(new Dimension(canvW, canvH));
+		canvas.revalidate();
+		panel.setPreferredSize(new Dimension(canvW, canvH));
+		panel.revalidate();
+		guiPanel.validate();
+		canvas.repaint();
+		matrix.repaint();
+	}
+
+	@Override
+	public void rotate(int angle)
+	{
 		if (stateManager.crtState == stateManager.pieceState)
 		{
 			Element selected = (Element) stateManager.pieceState.selectedElement.domElement.getElementsByTagName("g").item(0);
-			if (selected != null) {
+			if (selected != null)
+			{
 				String attr = selected.getAttribute("transform");
 				TransformTag tr = new TransformTag(attr);
 
-				if (tr.rotate == null) {
+				if (tr.rotate == null)
+				{
 					tr.rotate = new Rotate(angle, svgDimension, svgDimension);
-				} else {
+				} else
+				{
 					tr.rotate.x = svgDimension;
-					tr.rotate.y = svgDimension; 
+					tr.rotate.y = svgDimension;
 					tr.rotate.angle += angle;
 					tr.rotate.angle %= 360;
 				}
@@ -221,134 +189,118 @@ public class CanvasImpl extends Constant implements Canvas {
 				selected.setAttribute("transform", tr.toString());
 				tr.rotate.angle *= -1;
 				Node text = selected.getElementsByTagName("text").item(0);
-				((Element) text.getParentNode()).setAttribute("transform",tr.rotate.toString());
+				((Element) text.getParentNode()).setAttribute("transform", tr.rotate.toString());
 
-			}		
+			}
 		}
 	}
-	
+
 	@Override
-	public void openSVG(JPanel guiPanel, String fileName) throws IOException {
-		System.out.println(guiPanel);
+	public void openSVG(JPanel guiPanel, String fileName) throws IOException
+	{
 		domFactory = (SVGDocument) saxFactory.createDocument("file:///" + fileName);
 		setCanvasVariables(guiPanel, domFactory);
-		
-		
-//		canvas.createNewCanvas(gui.getCenterPanel());
-		
 	}
 
 	@Override
-	public void saveSVG(String fileName) throws TransformerException, IOException {
-	SVGRect bbox = ((SVGLocatable)domFactory.getDocumentElement()).getBBox();
-	System.out.println("wi: " + bbox.getWidth() + "  hei: "+ bbox.getHeight());
-		domFactory.getRootElement().setAttribute("height", ""+(bbox.getHeight()+200));
-		domFactory.getRootElement().setAttribute("width",""+(bbox.getWidth()+200));
-	
-		TransformerFactory transFactory = TransformerFactory.newInstance();
-		Transformer trans = transFactory.newTransformer();
-		trans.setOutputProperty(OutputKeys.INDENT, "yes");
+	public void saveSVG(String fileName)
+	{
+		SVGRect bbox;
+		BufferedWriter out;
+		String xmlString;
+		
+		try
+		{
+			bbox = ((SVGLocatable) domFactory.getDocumentElement()).getBBox();
+			domFactory.getRootElement().setAttribute("height", "" + (bbox.getHeight() + 200));
+			domFactory.getRootElement().setAttribute("width", "" + (bbox.getWidth() + 200));
 
-		StringWriter stringWriter = new StringWriter();
-		StreamResult result = new StreamResult(stringWriter);
-		DOMSource source = new DOMSource(domFactory);
-		trans.transform(source, result);
-		String xmlString = stringWriter.toString();
+			TransformerFactory transFactory = TransformerFactory.newInstance();
+			Transformer trans = transFactory.newTransformer();
+			trans.setOutputProperty(OutputKeys.INDENT, "yes");
 
-		BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
-		out.write(xmlString);
-		out.close();		
+			StringWriter stringWriter = new StringWriter();
+			StreamResult result = new StreamResult(stringWriter);
+			DOMSource source = new DOMSource(domFactory);
+			trans.transform(source, result);
+			xmlString = stringWriter.toString();
+
+			out = new BufferedWriter(new FileWriter(fileName));
+			out.write(xmlString);
+			out.close();
+			
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		} catch (TransformerException e)
+		{
+			e.printStackTrace();
+		}
 	}
-	
-	//TODO scos document	
-	public void addPiece(SVGDocument document, String fileName, int cursorX, int cursorY, String id, String value) throws IOException {
+
+	// TODO scos document
+	public void addPiece(SVGDocument document, String fileName, int cursorX, int cursorY, String id, String value) throws IOException
+	{
 		SVGDocument doc1 = (SVGDocument) Constant.saxFactory.createDocument(fileName);
 
 		doc1.getRootElement().setAttribute("id", id);
 
 		// adaug un tag group
 		Element g = doc1.createElementNS(Constant.svgNS, "g");
-		g.setAttribute("transform","translate(" + cursorX + "," + cursorY + ")");
+		g.setAttribute("transform", "translate(" + cursorX + "," + cursorY + ")");
 		g.appendChild(doc1.getRootElement());
 
 		// aici se unesc
 		Node x = document.importNode(g, true);
 		document.getDocumentElement().appendChild(x);
-		
-		
-		
-		//conventie piesa de intrare: 2 copii <g>: primul: piesa + textLabel; alDoilea: pinii
+
+		// conventie piesa de intrare: 2 copii <g>: primul: piesa + textLabel;
+		// alDoilea: pinii
 		setTextTag(x, id, value);
-		
+
 		EventTarget target = (EventTarget) document.getElementById(id);
 		target.addEventListener("mousedown", eventListener.mouseDownListener, true);
 		target.addEventListener("mouseup", eventListener.mouseUpListener, true);
 		target.addEventListener("click", eventListener.mouseClickListener, true);
 	}
-	
-	private static void setTextTag(Node x, String text1, String text2) {
+
+	private static void setTextTag(Node x, String text1, String text2)
+	{
 		NodeList nl = ((Element) x).getElementsByTagName("text");
-		for (int i = 0; i < nl.getLength(); i++) {
-			if(((Element)(nl.item(i).getFirstChild())).getAttribute("id").equals("name"))
-				((Element)nl.item(i)).setTextContent(text1);
-			else 
-				((Element)nl.item(i)).setTextContent(text2);
-		}	
+		for (int i = 0; i < nl.getLength(); i++)
+		{
+			if (((Element) (nl.item(i).getFirstChild())).getAttribute("id").equals("name"))
+				((Element) nl.item(i)).setTextContent(text1);
+			else
+				((Element) nl.item(i)).setTextContent(text2);
+		}
 	}
-	
+
 	@Override
-	public String getLastSelectedPieceId() {
+	public String getLastSelectedPieceId()
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
-	public Component getPreview(String fileName) {
+	public Component getPreview(String fileName)
+	{
 		CanvasPreview preview = new CanvasPreview(this);
 		return preview.getPreview(fileName);
 	}
 
 	@Override
-	public void addWire(String id, List<? extends Point> pointList) {
-		Wire wire = new Wire(this, pointList, id);
+	public void addWire(String id, List<? extends Point> pointList)
+	{
+		// Wire wire =
+		new Wire(this, pointList, id);
 	}
 
 	@Override
-	public void removeWire(String id) {
+	public void removeWire(String id)
+	{
 		Wire wire = new Wire(this, domFactory.getElementById(id));
 		wire.delete();
-	}
-}
-
-@SuppressWarnings("serial")
-class PanelScroll extends JPanel implements Scrollable {
-	@Override
-	public Dimension getPreferredScrollableViewportSize() {
-		// TODO Auto-generated method stub
-		return getPreferredSize();
-	}
-
-	@Override
-	public int getScrollableBlockIncrement(Rectangle visibleRect,
-			int orientation, int direction) {
-		return 96;
-	}
-
-	@Override
-	public boolean getScrollableTracksViewportHeight() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean getScrollableTracksViewportWidth() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public int getScrollableUnitIncrement(Rectangle visibleRect,
-			int orientation, int direction) {
-		return 24;
 	}
 }
--- trunk/final/src/edss/canvas/EventListenerImpl.java
+++ trunk/final/src/edss/canvas/EventListenerImpl.java
@@ -1,5 +1,6 @@
 package edss.canvas;
 
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
@@ -9,114 +10,121 @@ import java.awt.event.MouseMotionListener;
 import org.w3c.dom.events.Event;
 import org.w3c.dom.events.EventListener;
 
-
-public class EventListenerImpl {
+public class EventListenerImpl
+{
 	CanvasImpl canvas;
-	
-	public EventListenerImpl(CanvasImpl canvas) {
+
+	public EventListenerImpl(CanvasImpl canvas)
+	{
 		this.canvas = canvas;
 	}
-	
-	public EventListener mouseDownListener = new EventListener() {
+
+	public EventListener mouseDownListener = new EventListener()
+	{
 
 		@Override
-		public void handleEvent(Event evt) {
+		public void handleEvent(Event evt)
+		{
 			canvas.stateManager.getMouseDownElementListener(evt);
 		}
 	};
 
-	public EventListener mouseUpListener = new EventListener() {
+	public EventListener mouseUpListener = new EventListener()
+	{
 
 		@Override
-		public void handleEvent(Event evt) {
+		public void handleEvent(Event evt)
+		{
 			canvas.stateManager.getMouseUpElementListener(evt);
 		}
 	};
 
-	public EventListener mouseClickListener = new EventListener() {
+	public EventListener mouseClickListener = new EventListener()
+	{
 
 		@Override
-		public void handleEvent(Event evt) {
+		public void handleEvent(Event evt)
+		{
 			canvas.stateManager.getMouseClickElementListener(evt);
 		}
 	};
 
-	public EventListener mouseOutListener = new EventListener() {
+	public EventListener mouseOutListener = new EventListener()
+	{
 
 		@Override
-		public void handleEvent(Event evt) {
+		public void handleEvent(Event evt)
+		{
 			canvas.stateManager.getMouseOutElementListener(evt);
 		}
 	};
 
-	public EventListener mouseOverListener = new EventListener() {
+	public EventListener mouseOverListener = new EventListener()
+	{
 
 		@Override
-		public void handleEvent(Event evt) {
+		public void handleEvent(Event evt)
+		{
 			canvas.stateManager.getMouseOverElementListener(evt);
 		}
 	};
-	
-	public MouseMotionListener mouseMotionCanvas = new MouseMotionListener() {
+
+	public MouseMotionListener mouseMotionCanvas = new MouseMotionListener()
+	{
 
 		@Override
-		public void mouseMoved(MouseEvent arg0) {
+		public void mouseMoved(MouseEvent arg0)
+		{
 			canvas.stateManager.getMouseMovedListener(arg0);
 		}
 
 		@Override
-		public void mouseDragged(MouseEvent arg0) {
+		public void mouseDragged(MouseEvent arg0)
+		{
 			canvas.stateManager.getMouseDraggedListener(arg0);
 		}
 	};
 
-	public MouseListener mouseCanvas = new MouseListener() {
-
+	public MouseListener mouseCanvas = new MouseListener()
+	{
 		@Override
-		public void mouseReleased(MouseEvent arg0) {
+		public void mouseReleased(MouseEvent arg0)
+		{
 			canvas.stateManager.getMouseReleasedCanvasListener(arg0);
-
 		}
 
 		@Override
-		public void mousePressed(MouseEvent arg0) {
+		public void mousePressed(MouseEvent arg0)
+		{
 			canvas.stateManager.getMousePressedCanvasListener(arg0);
 		}
 
 		@Override
-		public void mouseExited(MouseEvent arg0) {
+		public void mouseExited(MouseEvent arg0)
+		{
 			canvas.stateManager.getMouseExitedCanvasListener(arg0);
 		}
 
 		@Override
-		public void mouseEntered(MouseEvent arg0) {
+		public void mouseEntered(MouseEvent arg0)
+		{
 			canvas.stateManager.getMouseEnteredCanvasListener(arg0);
 		}
 
 		@Override
-		public void mouseClicked(MouseEvent arg0) {
+		public void mouseClicked(MouseEvent arg0)
+		{
 			canvas.stateManager.getMouseClickedCanvasListener(arg0);
 		}
 	};
-	public KeyListener keyListener = new KeyListener() {
-		
+
+	public KeyListener keyListener = new KeyAdapter()
+	{
 		@Override
-		public void keyTyped(KeyEvent arg0) {
-			System.out.println("EventListenerImpl - KeyListener");
+		public void keyTyped(KeyEvent arg0)
+		{
 			canvas.stateManager.getKeyTypedListener(arg0);
 		}
-		
-		@Override
-		public void keyReleased(KeyEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-		
-		@Override
-		public void keyPressed(KeyEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
 	};
-	
+
 }
